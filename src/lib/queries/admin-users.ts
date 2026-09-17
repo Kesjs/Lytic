0c1dad73a36e7c94e4d80d6b7d9ee57ea5436022
Ken ken <95589932+Kesjs@users.noreply.github.com>
2026-09-17 09:28:41 +0200

    fix: change @tanstack/start to @tanstack/react-start in admin queries

--- src/lib/queries/admin-users.ts
+++ src/lib/queries/admin-users.ts
@@ -1,4 +1,4 @@
-import { createServerFn } from '@tanstack/start'
+import { createServerFn } from '@tanstack/react-start'
 import { getSupabaseServerClient } from '~/lib/supabase/server'
 import { requireAdmin } from '~/lib/queries/admin'
 
